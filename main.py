f66b53d4e940fd10aefe1596752e1e5f0fc6b395
apinklover <61984863+apinklover@users.noreply.github.com>
2021-05-03 00:41:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import random
+from discord.utils import get
+import urllib.parse, urllib.request, re
+import youtube_dl
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = ';', intents = intents)
+status = "monkaW"
 client.remove_command('help')
 
 @client.event # CONSOLE OUTPUT
 async def on_ready():
+    await client.change_presence(activity=discord.Game(status))
     print("GoonBot is up and working!")
 
 @client.event # CONSOLE OUTPUT
@@ -22,6 +27,12 @@ async def on_member_remove(member):
 async def on_command_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.CommandNotFound):
         await ctx.send("Unknown command. Maybe try `;help`?")
+    elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+        await ctx.send("Enter all the required arguments.")
+    elif isinstance(error, discord.ext.commands.errors.BadArgument):
+        await ctx.send("Make sure the argument passed in matches the required data type.")
+    elif isinstance(error, discord.ext.commands.errors.MissingPermissions):
+        await ctx.send("You do not have sufficient permissions to execute that command.")
 
 @client.command()
 async def ping(ctx):
@@ -49,24 +60,27 @@ async def _8ball(ctx, *, question):
                  "My sources say no.",
                  "Outlook not so good.",
                  "Very doubtful."]
-
     await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
 @client.command()
+@commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount = 5):
     await ctx.channel.purge(limit = amount + 1)
 
 @client.command()
+@commands.has_permissions(kick_members=True)
 async def kick(ctx, member : discord.Member, *, reason = None):
     await member.kick(reason = reason)
     await ctx.send(f"Kicked {member.mention}")
 
 @client.command()
+@commands.has_permissions(kick_members=True)
 async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
     await ctx.send(f"Banned {member.mention}")
 
 @client.command()
+@commands.has_permissions(ban_members=True)
 async def unban(ctx, *, member):
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split("#")
@@ -98,21 +112,31 @@ async def howpog(ctx):
         await ctx.send(f"You are {pog_meter}% pog. :PogChamp:")
 
 @client.command()
+async def ytlink(ctx, *, search_query):
+    query_string = urllib.parse.urlencode({"search_query": search_query})
+    htm_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r"watch\?v=(\S{11})", htm_content.read().decode())
+    await ctx.send("Here is your YouTube video:")
+    await ctx.send("http://www.youtube.com/watch?v=" + search_results[0])
+
+@client.command()
 async def help(ctx):
-    await ctx.send(f"**These are the current bot commands**\n"
+    await ctx.send(f"**These are the current bot commands. <required> [optional]**\n"
                    f"**BOT PREFIX =** `;`\n"
                    f"\n"
                    f"*Fun Commands*\n"
                    f"\n"
                    f"`ping` Reports latency in milliseconds.\n"
-                   f"`8ball <question>` An 8-Ball. Aliases are `_8ball` and `eightball`.\n"
+                   f"`8ball <string>` An 8-Ball. Aliases are `_8ball` and `eightball`.\n"
                    f"`ppsize` HOW BIG IS UR PP?\n"
+                   f"`howpog` Determines your poggers status, represented as a percentage.\n"
+                   f"`ytlink <string>` Gets first YouTube search result and sends its URL.\n"
                    f"\n"
                    f"*Administrative*\n"
                    f"\n"
-                   f"`clear <amount>` Delete specified amount of messages. If not specified, delete 5, excluding the command itself.\n"
-                   f"`kick <tag>` Kick specified member.\n"
-                   f"`ban <tag>` Ban specified member.\n"
-                   f"`unban <tag>` Unban specified member.")
+                   f"`clear [int]` Delete specified amount of messages. If not specified, delete 5.\n"
+                   f"`kick <@mention>` Kick specified member.\n"
+                   f"`ban <@mention> [string]` Ban specified member with an optional reason.\n"
+                   f"`unban <@mention> [string]` Unban specified member with an optional reason.")
 
 client.run(" ENTER BOT ADDRESS HERE ")
